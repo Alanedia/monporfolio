aa0c9d5be9f2cf2a399176f433aab282e3aafa8d
DIARRASSOUBA Chaliau Ronald Alane <alandiarrassouba@gmail.com>
2025-06-22 10:58:40 +0000

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -6,12 +6,17 @@ function showSection(sectionId) {
     document.getElementById(sectionId).classList.add("active");
   }
   
-  function toggleMenu() {
-    const navLinks = document.querySelector(".nav-links");
-    navLinks.classList.toggle("active");
+ function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
   }
   
-
+  document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+      document.querySelector('.nav-links').classList.remove('active');
+    })
+  })
+  
 function animateProgressBars() {
     setTimeout(() => {
         const progressBars = document.querySelectorAll(".progress-fill");
